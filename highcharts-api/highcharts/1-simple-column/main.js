81a53909135f8ac0ace6aa830ce480906c2379d4
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2022-08-17 20:12:31 +0200

    trying to assign a label to certain cols

--- highcharts-api/highcharts/1-simple-column/main.js
+++ highcharts-api/highcharts/1-simple-column/main.js
@@ -15,13 +15,12 @@ const createSeriesData = () => {
 
 const chartData = Array.from(Array(3)).map(()=>createSeriesData());
 
-const c = Highcharts.chart('container', {
+Highcharts.chart('container', {
     chart: {
         type: 'column',
 
         events: {
-            load(){ // this is how i found i could set the yAxis.max as required by the task
-                
+            load(){
                 let max = 0;
                 
                 this.series.forEach((s) => {
@@ -29,50 +28,37 @@ const c = Highcharts.chart('container', {
                         max = s.dataMax
                 });
 
-                this.yAxis[0].update({
-                    max: max * 2
-                })
+                console.log(this.yAxis[0].plotLinesAndBands);
 
+                this.yAxis[0].update({
+                    max: max * 2,
+                    plotLines: [{
+                        color: '#92C08A',
+                        width: 4, // also i added this to make the dashes more apparent
+                        value: 1.5 * max,
+                        dashStyle: "Dash",
+                    }],
+
+                })
+                
             }
         }
     },
 
-    title: null,
-    
-    plotOptions: {
-         series: {
-            
+    title: {
+        text:null,
+    },
+
+    plotOptions:{
+        series: {
             dataLabels: {
-                enabled: true,
-                format: 'MAX',
-            },
-
-            events: {
-                afterAnimate: function(){
-
-                    /*
-                        - i am trying to set the labels filter here, so that only the biggest col(s) have the "MAX" label
-                    */
-                    this.chart.options.plotOptions.series.dataLabels.filter = {
-                            property: 'y',
-                            operator: '==',
-                            
-                            value: function(){
-                                let max = 0;
-                                
-                                this.chart.series.forEach((series) => {
-                                    if(max < series.dataMax)
-                                        max = series.dataMax
-                                });
-
-                                return max;
-                            }
-                        }
-                        
-                    }
+                enabled:true,
+                formatter: function(){                
+                    if(this.point.y === (this.series.yAxis.max / 2))
+                        return "MAX";
                 }
-           }
-        
+            }
+        }
     },
 
     xAxis: {
@@ -83,22 +69,14 @@ const c = Highcharts.chart('container', {
         ],
     },
 
-    yAxis: {
-        
+    yAxis: {     
         endOnTick: false,   // we need to set endOnTick to false in order to disable rounding up of yAxis.max
         tickInterval: 2.5,  // and i set tickInterval to make the ticks on this chart fit the one shown in the screenshot 
-
-        plotLines: [{
-            color: '#92C08A',
-            width: 4, // also i added this to make the dashes more apparent
-            value: 1.5 * largest,
-            dashStyle: "Dash",
-        }],
     },
 
     series: [{
         name: 'Tokyo',
-        data: chartData[0]
+        data: chartData[0],
     }, {
         name: 'New York',
         data: chartData[1]
