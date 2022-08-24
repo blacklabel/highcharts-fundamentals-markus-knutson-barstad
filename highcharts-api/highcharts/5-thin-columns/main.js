9c34974fcccd46bf007b97369a1c1c8710eab434
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2022-08-24 12:21:21 +0200

    fixed style, dissabled accessibility

--- highcharts-api/highcharts/5-thin-columns/main.js
+++ highcharts-api/highcharts/5-thin-columns/main.js
@@ -1,14 +1,10 @@
-const dataMaker = (n,min,max) => {
-	d = [];
-
-	for(let i = 0; i < n; i++){
-		d.push(Math.random() * (max - min) + min);
-	}
-
-	return d;
-}
+const dataMaker = (n,min,max) => Array.from(Array(n)).map(()=> Math.random() * (max - min) + min);
 
 Highcharts.chart('container', {
+	accessibility: {
+		enabled: false
+	},
+
 	chart: {
 		type: 'column'
 	},
@@ -25,32 +21,30 @@ Highcharts.chart('container', {
         series: {
 			boostThreshold: 1,
 			color: 'red',
-			data: dataMaker(300,60,90),
-        },
+			data: dataMaker(300,60,90)
+        }
    	},
 
 	xAxis:[
 		{
-			left: "5%",
-			width: "10%",
+			left: '5%',
+			width: '8%',
 			
 			labels: {
 				enabled: false
 			},
 			
-			minorTickLength: 0,
-			tickLength: 0,
-
 			title: {
 				text: 'Bank 1'
-			}
-		},
-		{
-			left: "25%",
-			width: "10%",
+			},
 
 			minorTickLength: 0,
-			tickLength: 0,
+			tickLength: 0
+		},
+		{
+			left: '25%',
+			width: '8%',
+
 			labels: {
 				enabled: false
 			},
@@ -58,14 +52,15 @@ Highcharts.chart('container', {
 			title: {
 				text: 'Bank 2'
 			},
-			offset:0
+
+			offset:0,
+			minorTickLength: 0,
+			tickLength: 0
 		},
 		{
-			left: "45%",
-			width: "10%",
+			left: '45%',
+			width: '8%',
 			
-			minorTickLength: 0,
-			tickLength: 0,
 			labels: {
 				enabled: false
 			},
@@ -73,14 +68,15 @@ Highcharts.chart('container', {
 			title: {
 				text: 'Bank 3'
 			},
+
+			minorTickLength: 0,
+			tickLength: 0,
 			offset:0
 		},
 		{
-			left: "65%",
-			width: "10%",
+			left: '65%',
+			width: '8%',
 			
-			minorTickLength: 0,
-			tickLength: 0,
 			labels: {
 				enabled: false,
 			},
@@ -88,14 +84,15 @@ Highcharts.chart('container', {
 			title: {
 				text: 'Bank 4'
 			},
-			offset:0
+
+			offset:0,
+			minorTickLength: 0,
+			tickLength: 0
 		},
 		{
-			left: "85%",
-			width: "10%",
+			left: '85%',
+			width: '8%',
 			
-			minorTickLength: 0,
-			tickLength: 0,
 			labels: {
 				enabled: false
 			},
@@ -103,7 +100,10 @@ Highcharts.chart('container', {
 			title: {
 				text: 'Bank 5'
 			},
-			offset:0
+			
+			offset:0,
+			minorTickLength: 0,
+			tickLength: 0
 		}
 	],
 
@@ -116,19 +116,19 @@ Highcharts.chart('container', {
 
 	series: [
 		{
-			xAxis: 0,
+			xAxis: 0
 		},
 		{
-			xAxis: 1,
+			xAxis: 1
 		},
 		{
-			xAxis: 2,
+			xAxis: 2
 		},
 		{
-			xAxis: 3,
+			xAxis: 3
 		},
 		{
-			xAxis: 4,
+			xAxis: 4
 		}
 	]
 	
