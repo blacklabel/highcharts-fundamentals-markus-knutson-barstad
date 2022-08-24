2237ca75f7834f58798755c8bf4a86e9f1cb67ca
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2022-08-24 21:52:55 +0200

    fixed up a bit!

--- highcharts-api/highcharts/4-separate-axes/main.js
+++ highcharts-api/highcharts/4-separate-axes/main.js
@@ -1,187 +1,162 @@
-/*
-    Should i refactor things more in the render-function? 
-    There is a decent amount of repetition there.
-*/
+
+//middle of the chart, its initial value is half the size of the container
+let chartMid = 500;
 
 const dataSets = [
-    [50, 36, 50, 40, 14],
-    [17, 45, 55, 78, 17]
-  ];
-  const bgSets = dataSets.map(set => set.map(n => 100 - n));
-  
-  Highcharts.chart('container', {
-    chart: {
-      type: 'bar',
-      marginTop: 50,
+  [50, 36, 50, 40, 14],
+  [17, 45, 55, 78, 17]
+];
 
-      events: {
-        render: function() {
-          const ren = this.renderer;
-  
-          //axis-titles
-          if (this.customAxisTitles) {
-            this.customAxisTitles.forEach(l => l.destroy());
-          }
-  
-          this.customAxisTitles = [];
-          ['Manegerial positions', 'Non manegerial positions'].forEach((title, index) => {
-            this.customAxisTitles.push(ren.label(title, this.yAxis[index].pos + this.yAxis[index].width / 2, 0));
-          })
-  
-          this.customAxisTitles.forEach(l => {
-            l.attr({
-                align: 'center'
-              })
-              .css({
-                fontWeight: 'bold',
-                color: 'grey'
-              })
-              .add()
-          });
+const bgSets = dataSets.map(set => set.map(n => 100 - n));
 
-          // The middle labels
-          if (this.customLabels) {
-            this.customLabels.forEach(l => l.destroy());
-          }
+const BG_PROPS = {
+  color: '#A5B4C7',
+  enableMouseTracking: false,
+  dataLabels: {
+    enabled: false
+  }
+};
 
-          this.customLabels = [];
-          ['Dep1', 'Dep2', 'Dep3', 'Dep4', 'Dep5'].forEach((text, i) => {
-            this.customLabels.push(
-              ren.label(
-                text,
-                this.chartWidth / 2,
-                this.series[0].data[i].shapeArgs.x + 50 + (Math.abs(this.series[0].pointXOffset) / 3)
-              )
-            );
-          });
-  
-          this.customLabels.forEach(l => {
-            l.attr({
-                align: 'center'
-              })
-              .css({
-                fontSize: '11px',
-                color: 'grey'
-              })
-              .add();
-          });
-  
-          //a line hiding the last gridline of the right yAxis as shown in the screenshot
-          if (this.customHiderLine) {
-            this.customHiderLine.destroy();
-          }
-  
-          this.customHiderLine = ren.path([
-              'M', this.yAxis[1].pos + this.yAxis[1].width, this.xAxis[0].pos,
-              'L', this.yAxis[1].pos + this.yAxis[1].width, this.xAxis[0].pos + this.yAxis[0].height
-            ])
+const YAXIS_PROPS = {
+  tickPositions: [0,20,40,60,80,100], 
+  width: '40%',
+  max: 100,
+  title: {
+    text: ''
+  }
+};
+
+const clearScreen = (toClear) => {
+  if(toClear){
+    Array.from(toClear).forEach(obj => obj.destroy());
+  }
+}
+
+Highcharts.chart('container', {
+  accessibility: {
+    enabled: false
+  },
+
+  chart: {
+    type: 'bar',
+    marginTop: 50,
+
+    events: {
+      render: function() {
+        //clearing
+        clearScreen(this.customAxisTitles);
+        clearScreen(this.customHiderLine);
+
+        //declaring
+        chartMid = this.chartWidth / 2;
+        const ren = this.renderer;
+        this.customAxisTitles = [];
+        this.customLabels = [];
+
+        //hiderline
+        this.customHiderLine = ren.path([
+            'M', this.yAxis[1].pos + this.yAxis[1].width, this.xAxis[0].pos,
+            'L', this.yAxis[1].pos + this.yAxis[1].width, this.xAxis[0].pos + this.yAxis[0].height
+        ])
+        .attr({
+          'stroke-width': 5,
+          stroke: '#FFFFFF'
+        })
+        .add()
+        .toFront();
+
+        //custom titles
+        ['Manegerial positions', 'Non manegerial positions'].forEach((title, index) => {
+          this.customAxisTitles.push(
+            ren.label(title, this.yAxis[index].pos + this.yAxis[index].width / 2, 0)
             .attr({
-              'stroke-width': 5,
-              stroke: '#FFFFFF'
+              align: 'center'
             })
-            .add()
-            .toFront();
-        }
+            .css({
+              fontWeight: 'bold',
+              color: 'grey'
+            })
+          );
+        })
+        this.customAxisTitles.forEach(l => l.add());
+      }
+    }
+  },
+
+  title: {
+    text: ''
+  },
+
+  legend: {
+    enabled: false
+  },
+
+  xAxis: {
+    lineWidth: 0,
+    lineColor: 'transparent',
+    tickWidth:0,
+    labels: {
+      x: chartMid,
+      formatter: function () {
+        return "Dep" + (5 - this.pos);
+      }
+    }
+  },
+
+  yAxis: [{
+      left: '0%',
+      reversed: true,
+      ...YAXIS_PROPS
+    },
+    {  
+      left: '60%',
+      showLastLabel: false,
+      offset: 0,
+      ...YAXIS_PROPS
+    },
+  ],
+
+  plotOptions: {
+    series: {
+      stacking: 'normal',
+      
+      dataLabels: {
+        enabled: true,
+        color: 'white',
+        format: '{y}%'
+      }
+
+    }
+  },
+
+  series: [
+    //dummy data as backgrounds
+    {
+      data: bgSets[0],
+      yAxis: 0,
+      ...BG_PROPS
+    },
+    {
+      data: bgSets[1],
+      yAxis: 1,
+      ...BG_PROPS
+    },
+    //actual data
+    {
+      color: 'red',
+      data: dataSets[0],
+      yAxis: 0,
+      dataLabels: {
+        align: 'right'
       }
     },
-  
-    title: {
-      text: ''
-    },
-  
-    legend: {
-      enabled: false
-    },
-  
-    xAxis: {
-      visible: false
-    },
-  
-    yAxis: [{
-        width: '40%',
-        max: 100,
-        tickInterval: 20,
-        tickAmount: 6,
-  
-        left: '0%',
-        reversed: true,
-  
-        title: {
-          text: ''
-        }
-  
-      },
-      {
-        width: '40%',
-        max: 100,
-        tickInterval: 20,
-        tickAmount: 6,
-  
-        left: '60%',
-        reversed: false,
-  
-        showLastLabel: false,
-        offset: 0,
-  
-        title: {
-          text: ''
-        }
-      },
-    ],
-  
-    plotOptions: {
-      series: {
-        stacking: 'normal',
-        clip: false,
-  
-        dataLabels: {
-          enabled: true,
-          color: 'white',
-  
-          formatter: function() {
-            return this.y + '%';
-          }
-        }
-  
+    {
+      color: 'red',
+      data: dataSets[1],
+      yAxis: 1,
+      dataLabels: {
+        align: 'left'
       }
-    },
-  
-    series: [
-      //dummy data as backgrounds
-      {
-        color: '#A5B4C7',
-        data: bgSets[0],
-        yAxis: 0,
-        dataLabels: {
-          enabled: false
-        }
-      },
-      {
-        color: '#A5B4C7',
-        data: bgSets[1],
-        yAxis: 1,
-        dataLabels: {
-          enabled: false
-        }
-      },
-      //actual data
-      {
-        color: 'red',
-        data: dataSets[0],
-        yAxis: 0,
-        dataLabels: {
-          align: 'right'
-        }
-  
-      },
-      {
-        color: 'red',
-        data: dataSets[1],
-        yAxis: 1,
-        dataLabels: {
-          align: 'left'
-        }
-      },
-    ],
-  });
-  
+    }
+  ]
+});
