5f7752d4f2a16586f6817d96a42a0cf2e7aec9f7
MarkusBarstad <markus.knutson.barstad@highsoft.com>
2022-08-27 16:25:15 +0200

    multiple points, removing dots, clearer code

--- highcharts-api/highcharts/6-visible-points-zoom/main.js
+++ highcharts-api/highcharts/6-visible-points-zoom/main.js
@@ -1,59 +1,69 @@
 const dataMaker = () => Array.from(Array(100)).map(()=>Math.floor(Math.random()* 100)); //build ints
 const checkCoord = (n,min,max) => n >= min && n <= max;
-
+const clearScreen = (toClean) => {
+	if(toClean){
+		Array.from(toClean).forEach(c => c.destroy());
+	}
+}
 Highcharts.chart('container',{
-	accessibility: {
-		enabled: false
-	},
 
 	chart: {
 		type: 'line',
 		zoomType: 'xy',
-		panKey: 'meta',
 
 		events: {
 			render: function () {
-				const ren  = this.renderer;
-				const xMin = this.xAxis[0].min;
-				const xMax = this.xAxis[0].max;
-				const yMin = this.yAxis[0].min;
-				const yMax = this.yAxis[0].max;	
+				const chart = this;
+
+				const ren  = chart.renderer;
+				const xMin = chart.xAxis[0].min;
+				const xMax = chart.xAxis[0].max;
+				const yMin = chart.yAxis[0].min;
+				const yMax = chart.yAxis[0].max;	
 				let pCount = 0;
-				let highest;
+				let highestPoints = [];
 
-				
-				if(this.label){
-					this.label.destroy();
-				}
+				clearScreen(chart.label);
+				clearScreen(chart.highestLabels);
+				clearScreen(chart.dots);
 
-				if(this.highestLabel){
-					this.highestLabel.destroy();
-				}
+				chart.highestLabels = [];
+				chart.dots = [];
 
-				this.series[0].data.forEach((p)=> {
+				chart.series[0].data.forEach((p)=> {
 					if(checkCoord(p.x,xMin,xMax) && checkCoord(p.y, yMin, yMax)){
-						if(highest == null || p.y > highest.y){
-							highest = p;
-							
-						}		
+
+						console.log(p);
+
+						if(highestPoints[0] == null || p.y == highestPoints[0].y){
+							highestPoints.push(p);
+						}else if(p.y > highestPoints[0].y)
+						{
+							highestPoints = [p];
+						}
 						pCount++;
 					}
 				});
-
-				console.log(highest);
-
-				this.label = ren.label('visible points: ' + pCount, this.xAxis[0].pos, this.spacingBox.height).add();
+		
+				chart.label = ren.label('visible points: ' + pCount, chart.xAxis[0].pos, chart.spacingBox.height).add();
 				
-				console.log(this.xAxis[0]);
-				this.highestLabel = ren.label(
-					highest.y + '', 
-					highest.plotX + this.xAxis[0].left - 11,
-					highest.plotY + this.yAxis[0].top - 26)
-				.attr({aligh:'center'})
-				.css({color:'red'})
-				.add();
+				//use the index of the point to get the position of the corresponding tick-label
+				//do it for y as well, the right y-tick line and a proper offset
+				highestPoints.forEach(p => {
 
-				this.dot = ren.circle(highest.plotX + this.xAxis[0].left,this.xAxis[0].height + 47,4).attr({align:'center'}).css({color:'red'}).add().toFront();
+					chart.highestLabels.push(ren.label(
+						p.y + '', 
+						p.plotX + chart.xAxis[0].left - 11,
+						p.plotY + p.series.itemHeight
+					)
+					.attr({aligh:'right'})
+					.css({color:'red'}));
+
+					chart.dots.push(ren.circle(p.plotX + chart.xAxis[0].left,chart.xAxis[0].height + 52,4).attr({align:'center'}).css({color:'red'}));
+				});
+				
+				chart.highestLabels.forEach(l => l.add());
+				chart.dots.forEach(d => d.add().toFront());
 			}
 		}
 	},
