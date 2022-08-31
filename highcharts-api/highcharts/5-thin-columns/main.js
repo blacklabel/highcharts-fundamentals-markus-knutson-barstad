aa75342b6f85954fe78587412e9775c92cb0e80f
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2022-08-31 14:05:08 +0200

    better structure, removed some needless options!

--- highcharts-api/highcharts/5-thin-columns/main.js
+++ highcharts-api/highcharts/5-thin-columns/main.js
@@ -1,135 +1,57 @@
-const dataMaker = (n,min,max) => Array.from(Array(n)).map(()=> Math.random() * (max - min) + min);
+const dataMaker = (n, min, max) => Array.from(Array(n)).map(() => Math.random() * (max - min) + min);
+
+const getBank = (i) => {
+  return {
+    left: String(5 + (i * 20)) + '%',
+    width: '8%',
+    offset: 0,
+    labels: {
+      enabled: false
+    },
+    title: {
+      text: 'Bank ' + (i + 1)
+    },
+    tickLength: 0
+  }
+}
+
+const buildBanks = (n) => {
+  const banks = [];
+  for (let i = 0; i < n; i++) {
+    banks.push(getBank(i));
+  }
+  return banks
+};
+
+const buildSeriesArr = (n) => {
+  series = [];
+  for (let i = 0; i < n; i++) {
+    series.push({
+      xAxis: i
+    });
+  }
+  return series;
+}
 
 Highcharts.chart('container', {
-	accessibility: {
-		enabled: false
-	},
-
-	chart: {
-		type: 'column'
-	},
-
-	title: {
-		text: ''
-	},
-
-	legend: {
-		enabled: false
-	},
-
-	plotOptions: {
-        series: {
-			boostThreshold: 1,
-			color: 'red',
-			data: dataMaker(300,60,90)
-        }
-   	},
-
-	xAxis:[
-		{
-			left: '5%',
-			width: '8%',
-			
-			labels: {
-				enabled: false
-			},
-			
-			title: {
-				text: 'Bank 1'
-			},
-
-			minorTickLength: 0,
-			tickLength: 0
-		},
-		{
-			left: '25%',
-			width: '8%',
-
-			labels: {
-				enabled: false
-			},
-
-			title: {
-				text: 'Bank 2'
-			},
-
-			offset:0,
-			minorTickLength: 0,
-			tickLength: 0
-		},
-		{
-			left: '45%',
-			width: '8%',
-			
-			labels: {
-				enabled: false
-			},
-
-			title: {
-				text: 'Bank 3'
-			},
-
-			minorTickLength: 0,
-			tickLength: 0,
-			offset:0
-		},
-		{
-			left: '65%',
-			width: '8%',
-			
-			labels: {
-				enabled: false,
-			},
-
-			title: {
-				text: 'Bank 4'
-			},
-
-			offset:0,
-			minorTickLength: 0,
-			tickLength: 0
-		},
-		{
-			left: '85%',
-			width: '8%',
-			
-			labels: {
-				enabled: false
-			},
-
-			title: {
-				text: 'Bank 5'
-			},
-			
-			offset:0,
-			minorTickLength: 0,
-			tickLength: 0
-		}
-	],
-
-	yAxis:
-		{
-			max: 125,
-			min: 0,
-			tickInterval: 25
-		},
-
-	series: [
-		{
-			xAxis: 0
-		},
-		{
-			xAxis: 1
-		},
-		{
-			xAxis: 2
-		},
-		{
-			xAxis: 3
-		},
-		{
-			xAxis: 4
-		}
-	]
-	
-})
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: ''
+  },
+  legend: {
+    enabled: false
+  },
+  plotOptions: {
+    series: {
+      color: '#ff0000',
+      data: dataMaker(300, 60, 90)
+    }
+  },
+  xAxis: buildBanks(5),
+  yAxis: {
+    max: 125
+  },
+  series: buildSeriesArr(5)
+});
