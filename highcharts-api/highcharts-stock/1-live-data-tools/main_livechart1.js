ff40c9537d1fa3eae996a7d7bf173a0d09970848
MarkusBarstad <markus.knutson.barstad@highsoft.com>
2022-09-14 17:56:49 +0200

    started second chart

--- highcharts-api/highcharts-stock/1-live-data-tools/main_livechart1.js
+++ highcharts-api/highcharts-stock/1-live-data-tools/main_livechart1.js
@@ -1,70 +1,116 @@
 (function (H){
     const data ={
         prevLong: 0.0,
+		prevLat: 0.0,
 		left: [],
+		right: []
 	};
 	
     const yieldData = () => {
         Highcharts.getJSON('http://api.open-notify.org/iss-now.json', (resp) => {
-            
-            let currentPoint = parseFloat(resp.iss_position.longitude) - data.prevLong;
 
+			//left
             data.left.push([
                 parseFloat(resp.timestamp),
-                currentPoint
+                parseFloat(resp.iss_position.longitude) - data.prevLong
                 ]);
             data.prevLong = parseFloat(resp.iss_position.longitude);
             leftChart.series.forEach(s => s.setData(data.left.slice(2)));
+			
+			//right
+			data.right.push([
+                parseFloat(resp.timestamp),
+                parseFloat(resp.iss_position.latitude) - data.prevLat
+                ]);
+            data.prevLat = parseFloat(resp.iss_position.latitude);
+            rightChart.series.forEach(s => s.setData(data.right.slice(2)));
+			
         });
     }
 
-    const leftChart = H.stockChart('container', {
+    const leftChart = H.stockChart('container1', {
         title: {
             text: 'LEFT'
         },
 
         yAxis:[{
-            height: '30%',
-            offset: 0,
+            height: '10%',
         },
         {
-            height: '30%',
-            top: '30%',
+            height: '10%',
+            top: '10%',
         },
         {
-            height: '30%',
-            top: '60%',
+            height: '10%',
+            top: '20%',
         }],
-
-        series: [
-            {
+		
+		
+		plotOptions: {
+			series: {
+				dataGrouping: {
+					enabled: true,
+					groupAllPoints:true,
+					groupPixelWidth:100,
+				}
+			}
+		},
+        series: [{
                 yAxis:0,
                 dataGrouping:{
                     enabled:false,
                 }
-            },
-            {
-                yAxis:1,
-                
-                dataGrouping: {
-                    approximation: 'average',
-                    enabled: true,
-                    forced:true,
-
-                }
-            },
-            {
-                yAxis:2,
-                dataGrouping: {
-                    approximation: 'sum',
-                    enabled: true,
-                }
-            },
-        ]
+            }]
     });
+	
+	const rightChart = H.stockChart('container2', {
+	title: {
+		text: 'RIGHT'
+	},
 
+	yAxis:[{
+		height: '10%',
+	},
+	{
+		height: '10%',
+		top: '20%',
+	},
+	{
+		height: '10%',
+		top: '30%',
+	}],
+	plotOptions: {
+		series: {
+			dataGrouping: {
+				enabled: true,
+				groupAllPoints:true,
+				groupPixelWidth:100,
+			}
+		}
+	},
+	series: [{
+			yAxis:0,
+			dataGrouping:{
+				enabled:false,
+			}
+		},
+		{
+			yAxis:1,
+			dataGrouping: {
+				approximation: 'average',
+			}
+		},
+		{
+			yAxis:2,
+			dataGrouping: {
+				approximation: 'sum',
+			}
+		}]
+});
+	
+	
     setInterval(()=> {
         yieldData();
-    }, 300);
+    }, 2000);
 
 }(Highcharts));
