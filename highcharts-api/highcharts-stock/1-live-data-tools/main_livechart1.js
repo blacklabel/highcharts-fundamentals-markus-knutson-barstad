a39a70e0bca0a33b2658368c7bd2d4cffdbfdeaf
MarkusBarstad <markus.knutson.barstad@highsoft.com>
2022-09-15 19:22:48 +0200

    progress

--- highcharts-api/highcharts-stock/1-live-data-tools/main_livechart1.js
+++ highcharts-api/highcharts-stock/1-live-data-tools/main_livechart1.js
@@ -1,4 +1,8 @@
+//https://jsfiddle.net/BlackLabel/ahd74kqn/
+//https://jsfiddle.net/BlackLabel/ahd74kqn/
+
 (function (H){
+
     const data ={
         prevLong: 0.0,
 		prevLat: 0.0,
@@ -23,29 +27,40 @@
                 parseFloat(resp.iss_position.latitude) - data.prevLat
                 ]);
             data.prevLat = parseFloat(resp.iss_position.latitude);
-            rightChart.series.forEach(s => s.setData(data.right.slice(2)));
+            //rightChart.series.forEach(s => s.setData(data.right.slice(2)));
+			rightChart.series[0].setData(data.right.slice(2));
 			
         });
     }
 
     const leftChart = H.stockChart('container1', {
         title: {
-            text: 'LEFT'
+            text: 'LATITUDE'
         },
 
-        yAxis:[{
-            height: '10%',
-        },
-        {
-            height: '10%',
-            top: '10%',
-        },
-        {
-            height: '10%',
-            top: '20%',
-        }],
-		
-		
+        series: [{
+                yAxis:0,
+                dataGrouping:{
+                    enabled:false,
+                }
+            }]
+	});
+	
+	const rightChart = H.stockChart('container2', {
+		title: {
+			text: 'LONGITUDE'
+		},		
+		yAxis:[{
+			height: '33%',
+		},
+		{
+			height: '33%',
+			top: '33%',
+		},
+		{
+			height: '33%',
+			top: '66%',
+		}],
 		plotOptions: {
 			series: {
 				dataGrouping: {
@@ -55,59 +70,85 @@
 				}
 			}
 		},
-        series: [{
-                yAxis:0,
-                dataGrouping:{
-                    enabled:false,
-                }
-            }]
-    });
-	
-	const rightChart = H.stockChart('container2', {
-	title: {
-		text: 'RIGHT'
-	},
-
-	yAxis:[{
-		height: '10%',
-	},
-	{
-		height: '10%',
-		top: '20%',
-	},
-	{
-		height: '10%',
-		top: '30%',
-	}],
-	plotOptions: {
-		series: {
-			dataGrouping: {
-				enabled: true,
-				groupAllPoints:true,
-				groupPixelWidth:100,
+		stockTools:{
+			gui: {
+				buttons: [ 'indicators', 
+				'separator', 
+				'simpleShapes', 
+				'lines', 
+				'crookedLines', 
+				'measure', 
+				'advanced', 
+				'toggleAnnotations', 
+				'separator', 
+				'verticalLabels', 
+				'flags', 
+				'separator', 
+				'zoomChange', 
+				'fullScreen', 
+				'typeChange', 
+				'separator', 
+				'currentPriceIndicator', 
+				'saveChart', 
+				'customDataGroups'
+				],
+				
+				definitions: {
+					customDataGroups:{
+						symbol: "circle.svg"
+					}
+				}
 			}
-		}
-	},
-	series: [{
+			
+		},
+		series: [{
 			yAxis:0,
+			id: 'main-series',
 			dataGrouping:{
 				enabled:false,
-			}
-		},
-		{
+				}
+			},
+			{
 			yAxis:1,
+			type: 'sma',
+			linkedTo: 'main-series',
+			params: {
+				period: 2
+				},
 			dataGrouping: {
-				approximation: 'average',
-			}
-		},
-		{
-			yAxis:2,
+				firstAnchor: 'start',
+				lastAnchor: 'end'
+				}
+			},
+			{
+			type: 'macd',
+			linkedTo: 'main-series',
+			yAxis: 2,
 			dataGrouping: {
-				approximation: 'sum',
-			}
+				firstAnchor: 'start',
+				lastAnchor: 'end'
+				}
 		}]
-});
-	
+
+			/*
+			{
+				yAxis:1,
+				
+				dataGrouping: {
+					approximation: 'average',
+				}
+				
+				
+			{
+			{
+				yAxis:2,
+				dataGrouping: {
+					approximation: 'sum',
+				}
+			}
+			]
+			*/
+	});
 	
     setInterval(()=> {
         yieldData();
