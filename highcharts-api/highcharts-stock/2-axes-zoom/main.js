e81c32837a50fa7249968c2bdc242ba5e3ae19a2
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2022-09-21 23:31:20 +0200

    Update main.js

--- highcharts-api/highcharts-stock/2-axes-zoom/main.js
+++ highcharts-api/highcharts-stock/2-axes-zoom/main.js
@@ -1,36 +1,16 @@
-const data = [0,20,40,60,80,100,120];
-
-//const focal = 60;
-//console.log(this.yAxis[0],this.yAxis[0].series[0].data[3].plotY);
-
+const data = [-60,-40,-20,0,20,40,60]
 const extremeAround = (axis, len, focal) => {
 	axis.setExtremes(
-		focal - axis.toValue(len),
-		focal + axis.toValue(len),
+		focal - Math.abs(axis.toValue(len)),
+		focal + Math.abs(axis.toValue(len)),
 	);
 };
 
-let holdingClick = false; //whether user clicked
-//let yDir = 0;
-let yDimension = 0;
-let lastYPos = null;
-
-window.addEventListener('mousedown',() => holdingClick = true);
-window.addEventListener('mouseup',() => holdingClick = false);
-window.addEventListener('mouseover',(e) => {
-	if(holdingClick){
-		if(lastYPos === null){
-			lastYPos = e.y;
-			yDimension = 0;
-		}else{
-			yDimension = e.y - lastYPos;
-			
-		}
-		console.log(yDimension);
-	}else{
-		lastYPos = null;
-	}
-});
+/*
+	todo: flytt mouseover-detection til
+		- label mouseover
+		- yLine mouseover
+*/
 
 Highcharts.stockChart('container', {
 	chart:{
@@ -39,26 +19,57 @@ Highcharts.stockChart('container', {
 		},
 		events:{
 			load: function(){
-				this.yAxis[0].setExtremes(null,null);
+				this.holdingClick = false; //whether user is holding mousebutton clicked
+				this.yScale = 0; //space of the box the user dragged
+				this.mouseLastYPos = null; //where the users drag originated
+				this.minX = 0; //minimum-extreme for the xAxis
+
+				window.addEventListener('mousedown',() => this.holdingClick = true);
+				window.addEventListener('mouseup',() => this.holdingClick = false);
+				window.addEventListener('mouseover',(e) => {
+					if(this.holdingClick){
+						if(this.mouseLastYPos === null){
+							this.mouseLastYPos = e.y;
+							this.yScale = 0;
+						}else{
+							this.yScale = e.y - this.mouseLastYPos;
+						}
+					}else{
+						this.mouseLastYPos = null;
+					}
+				});
+
+				window.addEventListener('wheel', (e) => {
+					this.minX += e.deltaY * 0.01;
+					this.minX = Math.max(this.minX, 0);
+					this.xAxis[0].setExtremes(this.minX,null);
+				});
+
 			},
 			selection: function(e){
 				if(!e.resetSelection){
-					extremeAround(this.yAxis[0],e.y,yDimension,60);
+					extremeAround(this.yAxis[0],this.yScale + e.y,0);
 				}else{
 					this.yAxis[0].setExtremes(null,null);
 				}
+				console.log(this.yAxis[0].series[0].data[3].plotY);
 			}
 		}
 	},
 	title: {
 		text: ''
 	},
+	xAxis:{
+		minRange: 0.000001
+	},
 	yAxis:{
-
 		events:{
 			setExtremes:function(e){
 				if(!e.trigger){
-					this.update({min:e.min,max:e.max});
+					this.update({
+						min:e.min,
+						max:e.max
+					});
 				}else{
 					e.preventDefault();
 				}
@@ -68,5 +79,4 @@ Highcharts.stockChart('container', {
 	series: [{
 		data: data,
 	}]
-	
 });
