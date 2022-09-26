a337d753505b824910de4075713853f569028896
MarkusBarstad <110829288+MarkusBarstad@users.noreply.github.com>
2022-09-26 09:57:04 +0200

    Update main.js

--- highcharts-api/highcharts-stock/2-axes-zoom/main.js
+++ highcharts-api/highcharts-stock/2-axes-zoom/main.js
@@ -1,41 +1,41 @@
-/*remove and regenerate plotband whenever setting new extremes*/
-
 Highcharts.stockChart('container', {
 	chart:{
+		animation:false,
 		events:{
 			load: function(){
-				this.holdingClick = false; //whether user is holding mousebutton clicked
-				this.minX = 0; //minimum-extreme for the xAxis
-				this.yScaleAccelerator = 0.0002;
-								
-				window.addEventListener('mousedown',() => {
-					console.log("CLICKD");
-					this.holdingClick = true;
+				//vars
+				const chart = this;
+				chart.xMin = 0;
+				chart.xClicked =  false; //was the xAxis clicked?
+				chart.yClicked = false; //was the yAxis clicked?
+				chart.dragOrigin = null;
+
+				//Event-listeners
+				window.addEventListener('mousedown',(e) => {
+					chart.yClicked = true;
+					chart.dragOrigin = {x:e.x, y:e.y};
 				});
-								
+
 				window.addEventListener('mouseup',() => {
-					console.log("RELEASD");
-					this.holdingClick = false;
+					chart.xClicked = chart.yClicked = false;
+					chart.dragOrigin = null;
 				});
-
-				window.addEventListener('mousemove', (e) => {				
-					const old = this.yAxis[0].getExtremes();
-
-					if(this.holdingClick === true){
-						const newExtremeOffset = this.yAxis[0].toValue(e.y)*this.yScaleAccelerator / 10;
-						this.yAxis[0].setExtremes(old.min - newExtremeOffset, old.max + newExtremeOffset);
-						this.yScaleAccelerator = Math.max(this.yScaleAccelerator + 0.0002,1);
-					}else{
-						this.yScaleAccelerator = 0.00002;
+				
+				window.addEventListener('mousemove', (e) => {
+					if(chart.yClicked === true){
+						const old = chart.yAxis[0].getExtremes();
+						const newExtremeOffset = chart.yAxis[0].toValue(chart.dragOrigin.y - e.y) * 0.06;
+						chart.yAxis[0].setExtremes(old.min - newExtremeOffset, old.max + newExtremeOffset);
 					}
-				})
-
-				window.addEventListener('wheel', (e) => {
-					this.minX += e.deltaY * 0.01;
-					this.minX = Math.max(this.minX, 0);
-					this.xAxis[0].setExtremes(this.minX,null);
 				});
-				this.series[0].setData([-6000,-4000,-2000,0,2000,4000,6000]);
+				
+				window.addEventListener('wheel', (e) => {
+					chart.xMin += e.deltaY * 0.01;
+					chart.xMin = Math.max(chart.xMin, 0);
+					chart.xAxis[0].setExtremes(chart.xMin,null);
+				});
+
+				chart.series[0].setData([-6000,-4000,-2000,0,2000,4000,6000]);
 			},
 		}
 	},
@@ -49,22 +49,7 @@ Highcharts.stockChart('container', {
 		minRange: 0.000001,
 		startOnTick:false,
 		endOnTick: false,
-		plotBands:[{
-			from: -100000,
-			to: 100000,
-			color:"transparent",
-			
-			// events:{
-			// 	mousemove: function(e){
-			// 		const axis = this.axis;
-			// 		const old = axis.getExtremes();
 
-			// 		if(axis.chart.holdingClick === true){
-			// 			axis.setExtremes(old.min - axis.toValue(e.y), old.max + axis.toValue(e.y));
-			// 		}
-			// 	}
-			// }
-		}],
 		events:{
 			setExtremes:function(e){
 				this.update({
