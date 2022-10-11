58c74630e565d3c0b20028780bc223724494818c
MarkusBarstad <markus.knutson.barstad@highsoft.com>
2022-10-11 19:29:54 +0200

    cleaned a bit more! ;)

--- highcharts-api/highcharts-gantt/1-gantt-axes-groups/main.js
+++ highcharts-api/highcharts-gantt/1-gantt-axes-groups/main.js
@@ -15,22 +15,22 @@ Highcharts.ganttChart('container', {
     name: 'Project 1',
     data: [{
         name: 'Main',
-        pointWidth: 0,
+        pointWidth: 0
       },
       {
         name: 'First',
         start: 1560902400000,
-        end: 1561075200000,
+        end: 1561075200000
       }, {
         name: 'Second',
         start: 1560902400000,
-        end: 1561075200000,
+        end: 1561075200000
       }, {
         name: 'Second',
         start: 1561507200000,
-        end: 1561680000000,
+        end: 1561680000000
       }
     ]
-  }],
+  }]
 
 });
